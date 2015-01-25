ead75b1421b379bba6ec6ec2cdcd91faf9f0aac3
jodavidson <jodavidson@conversantmedia.com>
2015-01-25 16:22:18 -0600

    Agent can now be a deployable jar as a Jetty server

--- src/main/java/edu/depaul/agent/Agent.java
+++ src/main/java/edu/depaul/agent/Agent.java
@@ -8,17 +8,10 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.json.Json;
-import javax.json.JsonObject;
+//import javax.json.Json;
+//import javax.json.JsonObject;
 
 import org.apache.log4j.Logger;
-import org.hyperic.sigar.CpuInfo;
-import org.hyperic.sigar.Mem;
-import org.hyperic.sigar.NetInfo;
-import org.hyperic.sigar.NetInterfaceConfig;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
-
 import edu.depaul.maestro.service.MaestroService;
 
 /**
@@ -95,9 +88,9 @@ public class Agent implements Runnable {
 			}
 			
 			System.out.println(JsonData);
-			JsonObject jsonObject = Json.createReader(new java.io.StringReader(JsonData)).readObject();
-			System.out.println("Some data: " + jsonObject.get("name"));
-			System.out.println("Some data: " + jsonObject.get("namespace"));
+//			JsonObject jsonObject = Json.createReader(new java.io.StringReader(JsonData)).readObject();
+//			System.out.println("Some data: " + jsonObject.get("name"));
+//			System.out.println("Some data: " + jsonObject.get("namespace"));
 			
 			in.close();
 		
@@ -107,94 +100,5 @@ public class Agent implements Runnable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	
-				
-/*
-	private static final Logger logger = Logger.getLogger(Agent.class);
-	private Sigar sigar = new Sigar();
-	private MaestroService<Container> maestroService;
-	private Container container = new Container();
-	private String agentId;
-	private Mem mem;
-	private CpuInfo[] cpus;
-	private NetInfo netInfo;
-	private NetInterfaceConfig config;
-	
-	public void setMaestroService(MaestroService<Container> maestroService) {
-		this.maestroService = maestroService;
 	}
-	
-	public void setAgentId(String agentId) {
-		this.agentId = agentId;
-	}
-	
-	public void run() {
-		collectStatistics();
-	}
-
-	private void collectStatistics() {
-		getSigarStatistics();
-		setMem();
-		setCPU();
-		setDiskUsage();
-		setOperatingSystem();
-		setSystemInfo();
-		maestroService.store(container);
-	}
-
-	private void getSigarStatistics() {
-		try {
-			mem = sigar.getMem();
-			cpus = sigar.getCpuInfoList();
-			netInfo = sigar.getNetInfo();
-			config = sigar.getNetInterfaceConfig();
-		}
-		catch(SigarException se) {
-			logger.error(se.getMessage(), se);
-		}
-	}
-
-	private void setMem() {
-		// mem
-		container.setMemFree(mem.getFree());
-		container.setMemTotal(mem.getTotal());
-		container.setMemUsed(mem.getUsed());
-	}
-
-	private void setCPU() {
-		// cpu
-		container.setCpuCount(cpus.length);
-		container.setCpuModel(cpus[0].getModel());
-		container.setCpuVendor(cpus[0].getVendor());
-	}
-
-	private void setDiskUsage() {
-		// disk
-		File file = new File("/");
-		container.setDiskSpaceFree(file.getFreeSpace());
-		container.setDiskSpaceUsed(file.getTotalSpace() - file.getFreeSpace());
-		container.setDiskSpaceTotal(file.getTotalSpace());
-
-	}
-	private void setOperatingSystem() {
-
-		// os
-		container.setOsDataModel(System.getProperty("sun.arch.data.model"));
-		container.setOsDescription(System.getProperty("os.version"));
-		container.setOsName(System.getProperty("os.name"));
-
-	}
-	private void setSystemInfo() {
-		try {
-			// system info
-			container.setHostName(netInfo.getHostName());
-			container.setPrimaryIpAddress(sigar.getFQDN());
-			container.setPrimaryMacAddress(config.getHwaddr());
-			container.setAgentId(agentId);
-		}
-		catch(SigarException se) {
-			logger.error(se.getMessage(), se);
-		}
-
-	} */
 }
