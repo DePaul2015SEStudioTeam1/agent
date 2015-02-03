08708d6a3cd95af3562af558c4d0e74a02059a1e
jdavidson091 <jdavidson091@gmail.com>
2015-02-02 19:55:00 -0600

    Renamed Cadvisor to LogCollector, renamed some variables for clarity

--- src/main/java/edu/depaul/agent/Agent.java
+++ src/main/java/edu/depaul/agent/Agent.java
@@ -1,120 +1,22 @@
 package edu.depaul.agent;
 
-//import edu.depaul.maestro.service.MaestroService;
-
-/**
- * 
- * @author Deonte D Johnson
- *
- * edit - john davidson, 1/10/2015
- * edit - Deonte Johnson, 2/2/15
- */
 public class Agent implements Runnable {
 	
 	@Override
 	public void run() {
-			
 		//send cAdvisor's url location to start retrieving data
-		Cadvisor data = new Cadvisor("http://140.192.249.16:8890/api/v1.2/docker");
+		LogCollector data = new LogCollector("http://140.192.249.16:8890/api/v1.2/docker");
 				
 		//print a log for each container
 		for(int i = 0; i < data.getNumberOfContainers(); i++) {
-			ContainerLog log = data.CreateContainerLog(i);
+			ContainerLog log = data.createContainerLog(i);
 			System.out.println(log.toString());
 		}
 				
 		//retrieving data from an individual container
-		ContainerLog l = data.CreateContainerLog(0);
+		ContainerLog l = data.createContainerLog(0);
 		String id = l.getContainerID(); //add to DB
 		String cpu_limit = l.getCpuLimit(); //add to DB
 		String cpu_total = l.getCpuTotal(); //add to DB
-/*
-	private static final Logger logger = Logger.getLogger(Agent.class);
-	private Sigar sigar = new Sigar();
-	private MaestroService<Container> maestroService;
-	private Container container = new Container();
-	private String agentId;
-	private Mem mem;
-	private CpuInfo[] cpus;
-	private NetInfo netInfo;
-	private NetInterfaceConfig config;
-	
-	public void setMaestroService(MaestroService<Container> maestroService) {
-		this.maestroService = maestroService;
-	}
-	
-	public void setAgentId(String agentId) {
-		this.agentId = agentId;
-	}
-	
-	public void run() {
-		collectStatistics();
-	}
-
-	private void collectStatistics() {
-		getSigarStatistics();
-		setMem();
-		setCPU();
-		setDiskUsage();
-		setOperatingSystem();
-		setSystemInfo();
-		maestroService.store(container);
-	}
-
-	private void getSigarStatistics() {
-		try {
-			mem = sigar.getMem();
-			cpus = sigar.getCpuInfoList();
-			netInfo = sigar.getNetInfo();
-			config = sigar.getNetInterfaceConfig();
-		}
-		catch(SigarException se) {
-			logger.error(se.getMessage(), se);
-		}
-	}
-
-	private void setMem() {
-		// mem
-		container.setMemFree(mem.getFree());
-		container.setMemTotal(mem.getTotal());
-		container.setMemUsed(mem.getUsed());
-	}
-
-	private void setCPU() {
-		// cpu
-		container.setCpuCount(cpus.length);
-		container.setCpuModel(cpus[0].getModel());
-		container.setCpuVendor(cpus[0].getVendor());
-	}
-
-	private void setDiskUsage() {
-		// disk
-		File file = new File("/");
-		container.setDiskSpaceFree(file.getFreeSpace());
-		container.setDiskSpaceUsed(file.getTotalSpace() - file.getFreeSpace());
-		container.setDiskSpaceTotal(file.getTotalSpace());
-
-	}
-	private void setOperatingSystem() {
-
-		// os
-		container.setOsDataModel(System.getProperty("sun.arch.data.model"));
-		container.setOsDescription(System.getProperty("os.version"));
-		container.setOsName(System.getProperty("os.name"));
-
-	}
-	private void setSystemInfo() {
-		try {
-			// system info
-			container.setHostName(netInfo.getHostName());
-			container.setPrimaryIpAddress(sigar.getFQDN());
-			container.setPrimaryMacAddress(config.getHwaddr());
-			container.setAgentId(agentId);
-		}
-		catch(SigarException se) {
-			logger.error(se.getMessage(), se);
-		}
-
-	} */
 	}
 }
---
+++ src/main/java/edu/depaul/agent/LogCollector.java
@@ -0,0 +1,96 @@
+package edu.depaul.agent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import edu.depaul.agent.JsonDataRetrieval.CadvisorData;
+
+
+public class LogCollector {
+
+	private int numberOfContainers;
+	private JsonDataRetrieval data;
+	
+	public LogCollector(String _url) {
+		data = new JsonDataRetrieval(_url);
+		numberOfContainers = data.getContainerName().size();
+	}
+	
+	/**
+	 * 
+	 * @param i index of container in list of containers running
+	 * @return container at index
+	 */
+	public ContainerLog createContainerLog(int i) {
+		ArrayList<String> containerIds = data.getContainerName();
+		ArrayList<String> containerNames = data.getContainerAltName();
+		ArrayList<HashMap<String, String>> statisticMapList = new ArrayList<HashMap<String,String>>();
+		ArrayList<ContainerLog> logList = new ArrayList<ContainerLog>();
+		
+		//list to hold hash map data
+		statisticMapList.add(data.getContainerCpuLimit());
+		statisticMapList.add(data.getContainerCpuTotal());
+		statisticMapList.add(data.getContianerTimestamp());
+		statisticMapList.add(data.getContainerMemoryUsage());
+		statisticMapList.add(data.getContinerMemoryLimit());
+		statisticMapList.add(data.getContainerFileSystemCapacity());
+		statisticMapList.add(data.getContainerFileSystemUsage());
+		
+		//create a new log for each container
+		ContainerLog containerLog = new ContainerLog();
+		
+		//run through each hash map in the array list
+		for(HashMap<String,String> logStatisticMap : statisticMapList) {
+			logStatisticMap.remove(null);
+			
+			Set<Entry<String, String>> set = logStatisticMap.entrySet();
+			Iterator<Entry<String, String>> iterator = set.iterator();
+			
+			while(iterator.hasNext()) {
+				Map.Entry<String, String> _entry = (Map.Entry<String, String>)iterator.next();
+				
+				//get the containers alt id
+				containerLog.setID(containerNames.get(i));
+				
+				//get container id
+				containerLog.setContainerID(containerIds.get(i));
+				
+				if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.CL)) {
+					containerLog.setCpuLimit(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.CT)) {
+					containerLog.setCpuTotal(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.TIME)) {
+					containerLog.setTimeStamp(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.MU)) {
+					containerLog.setMemUsage(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.ML)) {
+					containerLog.setMemLimit(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.FSC)) {
+					containerLog.setFileSystemCapacity(_entry.getValue());
+					
+				} else if(_entry.getKey().equalsIgnoreCase(containerNames.get(i) + CadvisorData.FSU)) {
+					containerLog.setFileSystem_Usage(_entry.getValue());
+				}
+			}
+			logList.add(containerLog);
+		}
+		return logList.get(i);
+	}
+	
+	/**
+	 * 
+	 * @return number of containers running
+	 */
+	public int getNumberOfContainers() {
+		return numberOfContainers;
+	}
+}
