9e28f4c6b10efcd71ce7a7f791c0baa13bf1dfb4
Paul A. Trzyna <ptrzyna@gmail.com>
2015-02-08 21:57:26 -0600

    fixed unit tests

--- src/test/java/edu/depaul/agent/DeployerClientTest.java
+++ src/test/java/edu/depaul/agent/DeployerClientTest.java
@@ -4,18 +4,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import edu.depaul.agent.LocalDockerService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:beans/agent-config.xml")
 public class DeployerClientTest {
 
-	@Autowired
-	private LocalDockerService dockerService;
+	//@Autowired
+	//private LocalDockerService dockerService;
 
 	@Before
 	public void setUp() {
