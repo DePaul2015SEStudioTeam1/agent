bc31cf5af5fa1f98d3a7e5598e221de9914ba8d9
jdavidson091 <jdavidson091@gmail.com>
2015-02-16 20:19:23 -0600

    merged with development

--- src/main/java/edu/depaul/agent/Agent.java
+++ src/main/java/edu/depaul/agent/Agent.java
@@ -1,6 +1,5 @@
 package edu.depaul.agent;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -12,11 +11,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Agent {
 	
 	public static void main(String[] args) {
+		if(args == null || args.length == 0) {
+			System.err.println("Please supply 'armada.service.url'. Example: http://localhost:8083/");
+			System.exit(1);
+		}
+		
+		System.getProperties().setProperty("armada.service.url", args[0]);
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans/agent-config.xml");
 		LogCollectionTask logCollectionTask = (LogCollectionTask) context.getBean("logCollectionTask");
 		logCollectionTask.setAgentId((args == null || args.length < 1)? UUID.randomUUID().toString() : args[0]);
 
 		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-		executor.scheduleAtFixedRate(logCollectionTask, 3, 5, TimeUnit.SECONDS);
+		executor.scheduleAtFixedRate(logCollectionTask, 5, 5, TimeUnit.SECONDS);
 	}
 }
