f83a7cefcf3be5976097ba49ea0be37320718ff7
jdavidson091 <jdavidson091@gmail.com>
2015-02-16 20:13:59 -0600

    json parses all fields for AgentContainerLog now

--- src/main/java/edu/depaul/agent/LogCollector.java
+++ src/main/java/edu/depaul/agent/LogCollector.java
@@ -61,66 +61,93 @@ public class LogCollector {
 		 */
 		while (jsonParser.hasNext()) {
 			AgentContainerLog containerLog = new AgentContainerLog();
-			JsonParser.Event nextContainerId = jsonParser.next();
-			nextContainerId = jsonParser.next();
+//			JsonParser.Event nextContainerId = jsonParser.next();
+//			nextContainerId = jsonParser.next();
 			populateLog(containerLog);
 			logList.add(containerLog);
 		}
 	}
 
 	private static void populateLog(final AgentContainerLog containerLog) {
-		/**
-		 * JSON is structured like this...
-		 *
-		 *      { "containerId" :
-		 *          {   name : "name",
-		 *              aliases : [ "alias", "alias" ],
-		 *              namespace : "namespace",
-		 *              spec :
-		 *                  {
-		 *                  }
-		 *              stats : []
-		 *          }
-		 *        "containerId2" :
-		 *          {
-		 *              ...
-		 *          }
-		 *      }
-		 */
+
 		try {
-
-
-
-
-			containerLog.containerUniqueId = jsonParser.getString();
-
-			iterateParserUntilKeyMatch("timestamp");
-			nextJson(); //select timestamp value
-			String timestamp = jsonParser.getString();
-			Date date = DateUtils.parseDate(timestamp, new String[]{"yyyy-MM-dd hh:mm:ss.SSSZ"});
-			containerLog.timestamp = new Timestamp(date.getTime());
-
-			iterateParserUntilKeyMatch("cpu");
-			iterateParserUntilKeyMatch("total");
-			nextJson(); //select total cpu usage
-			containerLog.cpuTotal = jsonParser.getLong();
-
-			iterateParserUntilKeyMatch("memory");
-			iterateParserUntilKeyMatch("usage");
-			nextJson();
-			containerLog.memUsed = jsonParser.getLong();
-
-			iterateParserUntilKeyMatch("filesystem");
-			iterateParserUntilKeyMatch("usage");
-			nextJson();
-			containerLog.diskUsed = jsonParser.getLong();
-
+			//ordering of setting values is important
+			setContainerUniqueId(containerLog);
+			setContainerName(containerLog);
+			setContainerCpuLimit(containerLog);
+			setContainerMemoryLimit(containerLog);
+			setContainerTimestamp(containerLog);
+			setContainerCpuTotalUsage(containerLog);
+			setContainerMemoryUsage(containerLog);
+			setContainerFilesystemCapacity(containerLog);
+			setContainerFilesystemUsage(containerLog);
 
 			iterateParserUntilKeyMatch("//.*//.*");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
 
+	private static void setContainerUniqueId(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("name"); //"name" is the JSON field that actually holds the ID
+		nextJson();
+		containerLog.containerUniqueId = jsonParser.getString();
+	}
+
+	private static void setContainerName(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("aliases"); //"aliases" holds the human readable name
+		nextJson();
+		nextJson(); //iterate twice, since it is the beginning of an array
+		containerLog.containerUniqueId = jsonParser.getString();
+	}
+
+	private static void setContainerCpuLimit(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("cpu"); //"name" is the JSON field that actually holds the ID
+		iterateParserUntilKeyMatch("limit"); //"name" is the JSON field that actually holds the ID
+		nextJson();
+		containerLog.cpuTotal = jsonParser.getLong();
+	}
+
+	private static void setContainerMemoryLimit(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("memory"); //"name" is the JSON field that actually holds the ID
+		iterateParserUntilKeyMatch("limit"); //"name" is the JSON field that actually holds the ID
+		nextJson();
+		containerLog.memTotal = jsonParser.getLong();
+	}
+
+	private static void setContainerTimestamp(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("timestamp");
+		nextJson(); //select timestamp value
+		String timestamp = jsonParser.getString();
+		Date date = DateUtils.parseDate(timestamp, new String[]{"yyyy-MM-dd hh:mm:ss.SSSZ"});
+		containerLog.timestamp = new Timestamp(date.getTime());
+	}
+
+	private static void setContainerCpuTotalUsage(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("cpu");
+		iterateParserUntilKeyMatch("total");
+		nextJson(); //select total cpu usage
+		containerLog.cpuUsed = jsonParser.getLong();
+	}
+
+	private static void setContainerMemoryUsage(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("memory");
+		iterateParserUntilKeyMatch("usage");
+		nextJson();
+		containerLog.memUsed = jsonParser.getLong();
+	}
+
+	private static void setContainerFilesystemCapacity(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("filesystem");
+		iterateParserUntilKeyMatch("capacity");
+		nextJson();
+		containerLog.diskTotal = jsonParser.getLong();
+	}
+
+	private static void setContainerFilesystemUsage(AgentContainerLog containerLog) throws Exception {
+		iterateParserUntilKeyMatch("usage");
+		nextJson();
+		containerLog.diskUsed = jsonParser.getLong();
 	}
 
 	private static void iterateParserUntilKeyMatch(String regexKey) {
@@ -139,92 +166,4 @@ public class LogCollector {
 		return (jsonParser.hasNext()) ? jsonParser.next() : null;
 	}
 
-//	private int numberOfContainers;
-//	private JsonDataRetrieval data;
-//	private String url;
-//
-//	public LogCollector(String _url) {
-//		if(_url == null) {
-//			throw new IllegalArgumentException();
-//		}
-//		this.url = _url;
-//		data = new JsonDataRetrieval(_url);
-//		numberOfContainers = data.getContainerName().size();
-//	}
-//
-//	/**
-//	 *
-//	 * @param i index of container in list of containers running
-//	 * @return container at index
-//	 */
-//	public AgentContainer createContainerLog(int i) {
-//
-//		if((i >= numberOfContainers) || (i < 0))
-//			throw new IllegalArgumentException
-//			("index must be between 0 - " + (numberOfContainers-1));
-//
-//		//container unique id
-//		ArrayList<String> containerUniqueIds = data.getContainerName();
-//
-//		//retrieve container by alternative aliases
-//		ArrayList<String> containerNames = data.getContainerAltName();
-//		ArrayList<HashMap<String, String>> statisticMapList = new ArrayList<HashMap<String,String>>();
-//		ArrayList<AgentContainer> logList = new ArrayList<AgentContainer>();
-//
-//		statisticMapList.add(data.getContainerCpuLimit());
-//		statisticMapList.add(data.getContainerCpuTotal());
-//		statisticMapList.add(data.getContianerTimestamp());
-//		statisticMapList.add(data.getContainerMemoryUsage());
-//		statisticMapList.add(data.getContinerMemoryLimit());
-//		statisticMapList.add(data.getContainerFileSystemCapacity());
-//		statisticMapList.add(data.getContainerFileSystemUsage());
-//
-//		//create a new log for each container
-//		AgentContainer containerLog = new AgentContainer();
-//
-//		//run through each hash map in the array list
-//		for(HashMap<String,String> logStatisticMap : statisticMapList) {
-//			logStatisticMap.remove(null);
-//
-//			Set<Entry<String, String>> set = logStatisticMap.entrySet();
-//			Iterator<Entry<String, String>> iterator = set.iterator();
-//
-//			while(iterator.hasNext()) {
-//				Map.Entry<String, String> entry = (Map.Entry<String, String>)iterator.next();
-//
-//				//get the container(s) unique id
-//				containerLog.containerUniqueId = containerUniqueIds.get(i);
-//
-//				//get container short id
-//				containerLog.name = containerNames.get(i);
-//
-//				//send cAdvisor url
-//				containerLog.cAdvisorURL = url;
-//
-//				if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.CPU_TOTAL)) {
-//					containerLog.cpuTotal = new BigInteger(entry.getValue()).longValue();
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.TIMESTAMP)) {
-//					containerLog.timestamp = java.sql.Timestamp.valueOf(entry.getValue());
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.MEMORY_USAGE)) {
-//					containerLog.memUsed = new BigInteger(entry.getValue()).longValue();
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.FILESYSTEM_USAGE)) {
-//					containerLog.filesystemUsed = new BigInteger(entry.getValue()).longValue();
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.FILESYSTEM_CAPACITY)) {
-//					containerLog.filesystemTotal = new BigInteger(entry.getValue()).longValue();
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.MEMORY_LIMIT)) {
-//					//memTotal kept coming out to -1, this fixed the issue - DJ
-//					containerLog.memTotal = (long) Double.parseDouble(entry.getValue());//new BigInteger(entry.getValue()).longValue();
-//				} else if(entry.getKey().equalsIgnoreCase(containerNames.get(i) + JsonDataRetrieval.CAdvisorData.CPU_LIMIT)) {
-//					containerLog.cpuUsed = new BigInteger(entry.getValue()).longValue();
-//				}
-//			}
-//			logList.add(containerLog);
-//		}
-//		return logList.get(i);
-//	}
-//
-//
-//	public int getNumberOfContainers() {
-//		return numberOfContainers;
-//	}
 }
