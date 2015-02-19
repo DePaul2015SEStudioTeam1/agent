ab385f3d9450509170511b8b1689bff98b6a8709
Deonte D Johnson <Jet2kus84@gmail.com>
2015-02-19 02:10:34 -0600

    wrote new test for LogCollector

--- src/test/java/edu/depaul/agent/LogCollectorTest.java
+++ src/test/java/edu/depaul/agent/LogCollectorTest.java
@@ -1,120 +1,52 @@
 package edu.depaul.agent;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import edu.depaul.armada.model.AgentContainerLog;
 
 public class LogCollectorTest {
 
-//	LogCollector log = new LogCollector("http://140.192.249.16:8890/api/v1.2/docker");
-//
-//	@Test
-//	public void createContainerLogTest() {
-//
-//		//write a test for null argument
-//
-//		boolean instance = false;
-//
-//		AgentContainer l = log.createContainerLog(0);
-//
-//		if(l instanceof AgentContainer) instance = true;
-//
-//		Assert.assertTrue(instance);
-//	}
-//
-//	@Test
-//	public void exceedIndexTest() {
-//
-//		boolean thrown = false;
-//
-//		//check if exception is thrown if index isn't within range
-//		//exception should be thrown if index is greater than # of containers
-//		try {
-//			log.createContainerLog(6);
-//		} catch(IllegalArgumentException e) { thrown = true; }
-//		Assert.assertTrue(thrown);
-//
-//		//exception should be thrown if index is less than # of containers
-//		thrown = false;
-//		try {
-//			log.createContainerLog(-1);
-//		} catch(IllegalArgumentException e) { thrown = true; }
-//		Assert.assertTrue(thrown);
-//
-//		//check that exception isn't thrown on an index within range
-//		thrown = false;
-//		try {
-//			for(int i = 0; i < log.getNumberOfContainers(); i++)
-//				log.createContainerLog(i);
-//
-//		} catch(IllegalArgumentException e) {
-//			thrown = true;
-//		}
-//		Assert.assertFalse(thrown);
-//	}
-//	@Test
-//	public void ArgumentNotNullTest() {
-//		LogCollector l = null;
-//		boolean thrown = false;
-//
-//		try {
-//			 l = new LogCollector(null);
-//		} catch (IllegalArgumentException e) {
-//			thrown = true;
-//		}
-//
-//		Assert.assertTrue(thrown);
-//	}
-//
-//	@Test
-//	public void MalformedURLExceptionTest() { //remove test and give to dataRetrieval
-//
-//		boolean thrown = false;
-//
-//		try {
-//
-//			//only here to remove unreachable error
-//			new java.net.URL("www.ccc.edu"); //unused
-//
-//			new LogCollector("afsdfasd");
-//		} catch(MalformedURLException e) {
-//			thrown = true;
-//		}
-//
-//		Assert.assertTrue(thrown);
-//
-//	}
-//
-//	@Test
-//	public void numberOfContainersTest() {
-//
-//		AgentContainer[] logArray = new AgentContainer[log.getNumberOfContainers()];
-//
-//		for(int i = 0; i < log.getNumberOfContainers(); i++)
-//			logArray[i] = log.createContainerLog(i);
-//
-//		Assert.assertEquals(logArray.length, log.getNumberOfContainers());
-//	}
-//
-//	@Test
-//	public void dataFieldsInitializationTest() {
-//
-//		for(int i = 0; i < log.getNumberOfContainers(); i++) {
-//
-//			AgentContainer dataLog = log.createContainerLog(i);
-//
-//			//check if any of the data is initialized to null
-//			Assert.assertNotNull(dataLog.cAdvisorURL);
-//			Assert.assertNotNull(dataLog.name);
-//			Assert.assertNotNull(dataLog.containerUniqueId);
-//			Assert.assertNotNull(dataLog.timestamp);
-//
-//			//check that values are greater than 0
-//			Assert.assertTrue((dataLog.memTotal >= 0) ? true : false);
-//			Assert.assertTrue((dataLog.memUsed >= 0) ? true : false);
-//			Assert.assertTrue((dataLog.cpuTotal >= 0) ? true : false);
-//			Assert.assertTrue((dataLog.cpuUsed >= 0) ? true : false);
-//			Assert.assertTrue((dataLog.filesystemTotal >= 0) ? true : false);
-//			Assert.assertTrue((dataLog.filesystemUsed >= 0) ? true : false);
-//
-//		}
-//	}
+	@Test
+	public void ListNotEmptyTest() {
+		//"http://140.192.249.16:8890/api/v1.2/docker"
+		LogCollector log = new LogCollector();
+		log.setcAdvisorURL("http://140.192.249.16:8890/api/v1.2/docker");
+		java.util.List<AgentContainerLog> list = log.getCurrentLogs();
+
+		Assert.assertFalse(list.isEmpty());
+	}
+	
+	@Test
+	public void emptyCollectionTest() {
+		//"http://140.192.249.16:8890/api/v1.2/docker"
+		LogCollector log = new LogCollector();
+		log.setcAdvisorURL("List should be empty");
+		java.util.List<AgentContainerLog> list = log.getCurrentLogs();
+	
+		Assert.assertTrue(list.isEmpty());
+	}
+	
+	@Test
+	public void checkFieldInitializationTest() {
+		LogCollector log = new LogCollector();
+		log.setcAdvisorURL("http://140.192.249.16:8890/api/v1.2/docker");
+		java.util.List<AgentContainerLog> list = log.getCurrentLogs();
+		
+		for (AgentContainerLog l : list) {
+			Assert.assertNotNull(l.containerUniqueId);
+			Assert.assertNotNull(l.name);
+			Assert.assertTrue(l.cpuTotal > 0);
+			Assert.assertTrue(l.memTotal > 0);
+			Assert.assertNotNull(l.timestamp);
+			Assert.assertTrue(l.cpuUsed > 0);
+			Assert.assertTrue(l.memUsed > 0);
+			Assert.assertTrue(l.diskTotal > 0);
+			Assert.assertTrue(l.diskUsed > 0);
+			
+			
+		}
+	}
 
 }
